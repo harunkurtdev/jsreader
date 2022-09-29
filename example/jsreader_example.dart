44ac66e736db3a42c449f78119eefad784d911c0
harunkurtme <harunlakodla@gmail.com>
2022-09-29 16:34:50 +0300

    first commit

---
+++ example/jsreader_example.dart
@@ -0,0 +1,6 @@
+import 'package:jsreader/jsreader.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ lib/jsreader.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library jsreader;
+
+export 'src/jsreader_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
